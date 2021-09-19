a95228d1a50880bdb69dfdcd26ef825d9c0c32fe
Aaron Gerig <a.gerig@w-vision.ch>
2021-09-19 17:16:20 +0200

    feat: added tailwindui support to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   variants: {
     extend: {}
